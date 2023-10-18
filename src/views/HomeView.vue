ea682ea713218b721315745eb23247f0a282ab6f
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-10-18 14:14:46 +0200

    Подправил Импорт NavBar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,5 @@
 <script setup>
-import Navbar from '@/components/Navbar.vue'
-
-// import CardsSection from '@/components/blocks/CardsSection.vue'
-// import CaseSection from '@/components/blocks/CaseSection/CaseSection.vue'
-// import HeroSection from '@/components/blocks/HeroSection.vue'
-// import SecondSection from '@/components/blocks/SecondSection.vue'
-// import TeamSection from '@/components/blocks/TeamSection/TeamSection.vue'
-// import TickerSection from '@/components/blocks/TickerSection.vue'
-
-// import FooterSection from '@/components/blocks/FooterSection.vue'
+import Navbar from '@/components/NavBar.vue'
 </script>
 
 <template>
